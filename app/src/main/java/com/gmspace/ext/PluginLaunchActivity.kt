212f4131352fb9da73a946000a7812f430e068ab
iichen <475539335@qq.com>
2024-08-02 11:36:55 +0800

    微调

--- app/src/main/java/com/gmspace/ext/PluginLaunchActivity.kt
+++ app/src/main/java/com/gmspace/ext/PluginLaunchActivity.kt
@@ -1,27 +1,24 @@
 package com.gmspace.ext
 
-import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import com.gmspace.sdk.GmSpaceObject
 import com.gmspace.sdk.R
-import com.gmspace.sdk.proxy.GmSpaceUtils
-import com.vlite.sdk.context.HostContext
+import com.gmspace.sdk.model.AppItemEnhance
+import com.vlite.sdk.utils.GsonUtils
 
 class PluginLaunchActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_activity_launch)
 
-        // 需要处理未安装（安装了 但是后续插件被手动卸载再安装那么这里就获取不到之前安装的）
-        intent.getStringExtra("mPackageName")?.apply {
-            val pm: PackageManager = HostContext.getContext().packageManager
-            GmSpaceUtils.getPackageInfo(this,0)?.applicationInfo?.let { applicationInfo ->
-                findViewById<TextView>(R.id.iv_app_name).text = applicationInfo.loadLabel(pm).toString()
-                findViewById<ImageView>(R.id.iv_app_icon).setImageDrawable(applicationInfo.loadIcon(pm))
-            }
-        }
+//        intent.getStringExtra("mAppItem")?.apply {
+//            val appItem = GsonUtils.toObject(this, AppItemEnhance::class.java)
+//            findViewById<TextView>(R.id.iv_app_name).text = appItem.appName
+//            findViewById<ImageView>(R.id.iv_app_icon).setImageBitmap(BitmapFactory.decodeFile(appItem.iconExtUri))
+//        }
 
         GmSpaceObject.call32BitAppLaunchApplication(this)
     }
